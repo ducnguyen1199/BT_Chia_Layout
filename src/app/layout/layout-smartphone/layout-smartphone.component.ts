16a01a5dacf99a980e87500649e744293a0c8c20
CongDuc <conduc1199@gmail.com>
2019-08-06 17:04:18 +0700

    __

--- src/app/layout/layout-smartphone/layout-smartphone.component.ts
+++ src/app/layout/layout-smartphone/layout-smartphone.component.ts
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout-smartphone.component.scss']
 })
 export class LayoutSmartphoneComponent implements OnInit {
-  public DL:object=[
-    {src:"./assets/img/sp_iphoneX.png",title:"iPhone X",text:"iPhone X features a new all-screen design. Face ID, which makes your face your password"},
-    {src:"./assets/img/sp_note7.png",title:"Galaxy Note7",text:"The Galaxy Note7 comes with a perfectly symmetrical design for good reason"},
-    {src:"./assets/img/sp_vivo850.png",title:"Vivo",text:"A young global smartphone brand focusing on introducing perfect sound quality"},{src:"./assets/img/sp_blackberry.png",title:"Vivo",text:"A young global smartphone brand focusing on introducing perfect sound quality"}
-  ];
+  
   constructor() { }
 
   ngOnInit() {
